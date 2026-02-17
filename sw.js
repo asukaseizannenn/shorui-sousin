1e2271e7b319727f9354a1648c5ebbbadf9ce84c
asukaseizannenn <rihabirideiasuka4@gmail.com>
2026-02-17 14:57:34 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'doc-upload-v1';
+const CACHE = 'doc-upload-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -32,3 +32,4 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(fetch(e.request));
 });
+
